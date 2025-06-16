767962188909e9ca7a7341d1575ac2970284acc4
tunvi-006 <tunvi849@gmail.com>
2025-06-16 19:19:31 +0530

    Refactor index.php to include arithmetic and comparison operators examples

--- index.php
+++ index.php
@@ -29,6 +29,68 @@ var_dump($x);
 define("NUM",200);  // constant name must be quoted string
 echo NUM; 
 $b=NUM+20;
-echo $b;  
-echo "hi guys";       ;
+echo "<br>". $b;
+echo "<br>"."hi ji";     
+
+// arithmetic operators
+
+$a=10;
+$b=20;
+echo "<br>" . ($a + $b);
+echo "<br>" . ($a - $b);
+echo "<br>" . ($a * $b);
+echo "<br>" . ($a / $b);
+echo "<br>" . ($a % $b);
+echo "<br>" . ($a ** $b);
+
+// assignment operators
+$x = 10;
+echo "<br>Original x = $x<br>";
+
+$x += 5;
+echo "After += : $x<br>";
+
+$x -= 2;
+echo "After -= : $x<br>";
+
+$x *= 3;
+echo "After *= : $x<br>";
+
+$x /= 4;
+echo "After /= : $x<br>"; //  $x = $x % 4;
+
+
+$name = "Hello";
+$name .= " Taran!";
+echo $name;
+$a = 10;
+$b = "10";
+$c = 20;
+
+echo "<h3>Comparison Operators in PHP</h3>";
+
+echo "1. Equal (==): ";
+var_dump($a == $b);  // true, because values are equal (type not checked)
+
+echo "<br>2. Identical (===): ";
+var_dump($a === $b); // false, because types are different (int vs string)
+
+echo "<br>3. Not Equal (!=): ";
+var_dump($a != $c);  // true, 10 is not equal to 20
+
+
+echo "<br>5. Not Identical (!==): ";
+var_dump($a !== $b); // true, because types are different
+
+echo "<br>6. Greater Than (>): ";
+var_dump($c > $a);   // true, 20 > 10
+
+echo "<br>7. Less Than (<): ";
+var_dump($a < $c);   // true, 10 < 20
+
+echo "<br>8. Greater Than or Equal (>=): ";
+var_dump($a >= 10);  // true
+
+echo "<br>9. Less Than or Equal (<=): ";
+var_dump($a <= 9);   // false
 ?>
